82a568fade734b72d3500754e7491eb573cc8425
alvamo14 <morenoalvaro2004@gmail.com>
2025-03-24 11:54:52 +0100

    arreglado

--- Practica_1/includes/config.php
+++ Practica_1/includes/config.php
@@ -1,10 +1,6 @@
 <?php
 
 require_once __DIR__.'/Aplicacion.php';
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
 
 /**
  * Parámetros de conexión a la BD (local, con XAMPP)
