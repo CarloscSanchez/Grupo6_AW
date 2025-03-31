59d53a59a94d4627861b366ff043a1544bdc836f
MrL00P <117200493+Mr-Loop@users.noreply.github.com>
2025-03-31 12:37:28 +0200

    Update verLibro.php

--- Practica_1/verLibro.php
+++ Practica_1/verLibro.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__.'/includes/config.php';
-require_once __DIR__.'/includes/clases/productos/Libro.php';
-require_once __DIR__.'/includes/clases/usuarios/Usuario.php';
+require_once __DIR__.'/includes/clases/productos/libro.php';
+require_once __DIR__.'/includes/clases/usuarios/usuario.php';
 
 $id_libro = $_GET["id"] ?? null;
 
