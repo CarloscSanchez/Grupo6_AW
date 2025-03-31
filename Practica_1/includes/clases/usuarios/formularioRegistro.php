4c8ea7440bd36983e0161ed24f2f66c516996782
MrL00P <117200493+Mr-Loop@users.noreply.github.com>
2025-03-31 12:29:22 +0200

    Update formularioRegistro.php

--- Practica_1/includes/clases/usuarios/formularioRegistro.php
+++ Practica_1/includes/clases/usuarios/formularioRegistro.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__.'/../../Formulario.php';
-require_once __DIR__.'/Usuario.php';
+require_once __DIR__.'/../../formulario.php';
+require_once __DIR__.'/usuario.php';
 
 class FormularioRegistro extends Formulario
 {
